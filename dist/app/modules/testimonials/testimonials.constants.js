21e29921cb9f2fd680a9c13bb165e44f8f573bbe
akram-sakib <sayedakramsakib@gmail.com>
2024-04-14 18:17:02 +0600

    :fire: included build folder

---
+++ dist/app/modules/testimonials/testimonials.constants.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testimonialsSearchableFields = exports.testimonialsFilterableFields = void 0;
+exports.testimonialsFilterableFields = [
+    'search',
+    'description',
+    'userId',
+];
+exports.testimonialsSearchableFields = ['description'];
