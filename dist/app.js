4b4437ac1e0d8f238ff2fa361c64b38ec08d3428
akram-sakib <sayedakramsakib@gmail.com>
2024-04-14 18:41:35 +0600

    hardcoded url

--- dist/app.js
+++ dist/app.js
@@ -10,6 +10,7 @@ const express_session_1 = __importDefault(require("express-session"));
 const http_status_1 = __importDefault(require("http-status"));
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const routes_1 = __importDefault(require("./app/routes"));
+// import config from './config';
 const app = (0, express_1.default)();
 app.use((0, express_session_1.default)({
     resave: false,
@@ -19,7 +20,7 @@ app.use((0, express_session_1.default)({
 // Define the allowed origins
 // const allowedOrigins = [config.frontend_url, 'http://localhost:3000'];
 const corsOptions = {
-    origin: "*",
+    origin: "https://online-work-platform.vercel.app",
     credentials: true, //access-control-allow-credentials:true
 };
 app.use((0, cors_1.default)(corsOptions));
