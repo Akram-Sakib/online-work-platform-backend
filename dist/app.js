8d2fa4e0f89221b718221e1241dbf76cdd4c8613
akram-sakib <sayedakramsakib@gmail.com>
2024-04-14 18:37:17 +0600

    :egg: removed cors individual url

---
+++ dist/app.js
@@ -0,0 +1,47 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const express_session_1 = __importDefault(require("express-session"));
+const http_status_1 = __importDefault(require("http-status"));
+const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
+const routes_1 = __importDefault(require("./app/routes"));
+const app = (0, express_1.default)();
+app.use((0, express_session_1.default)({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET',
+}));
+// Define the allowed origins
+// const allowedOrigins = [config.frontend_url, 'http://localhost:3000'];
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use((0, cookie_parser_1.default)());
+//parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use('/api/v1', routes_1.default);
+//global error handler
+app.use(globalErrorHandler_1.default);
+//handle not found
+app.use((req, res, next) => {
+    res.status(http_status_1.default.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API Not Found',
+            },
+        ],
+    });
+    next();
+});
+exports.default = app;
---
+++ dist/app/modules/task/task.constants.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.taskSearchableFields = exports.taskFilterableFields = void 0;
+exports.taskFilterableFields = [
+    'search',
+    'title',
+    'description',
+    'slug',
+];
+exports.taskSearchableFields = ['title', 'description'];
